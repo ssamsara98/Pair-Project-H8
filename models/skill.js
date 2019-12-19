d71fead972ced9f70a17808b914ea86fb9333ead
dimas wicaksono <novan.top@gmail.com>
2019-12-20 01:19:12 +0700

    :alien:

--- models/skill.js
+++ models/skill.js
@@ -2,14 +2,23 @@
 module.exports = (sequelize, DataTypes) => {
   const Model = sequelize.Sequelize.Model;
   class Skill extends Model { }
-  Skill.init({
-    SkillName: {
-      type: DataTypes.STRING
+  Skill.init(
+    {
+      SkillName: {
+        type: DataTypes.STRING
+      }
+    },
+    {
+      hooks: {
+        beforeCreate: (uppercase, options) => {
+          // do stuff
+          uppercase.SkillName = uppercase.SkillName.toUpperCase()
+        }
+      },
+      sequelize
     }
-  }, {
-    sequelize
-  });
-  Skill.associate = function(models) {
+  );
+  Skill.associate = function (models) {
     // associations can be defined here
     Skill.belongsToMany(models.User, { through: models.UserSkill })
   };
